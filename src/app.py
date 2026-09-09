b51148b25677733587aef64ff6168014fcd8c79d
Mel2812 <lunap@Meli.titimeli.local>
2026-09-09 08:30:43 -0600

    Se agrego al archivo app.py el codigo de sistema Riego por goteo subteraneo

--- src/app.py
+++ src/app.py
@@ -1,11 +1,11 @@
-def percibe_funcion(Humedadsuelo, evapotranspiracion, clima):
+def percibe_funcion(mumedadsuelo, evapotranspiracion, clima):
 # codigo
  pass
 
-def decide_funcion(Volumen, momento):
+def decide_funcion(volumen, momento):
 # codigo
  pass
 
-def acciona_funcion(Abrevalvulas):
+def acciona_funcion(abre_valvulas):
 # codigo
  pass
